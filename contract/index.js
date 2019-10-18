cd5ee2e978f71c64f71394438377d9715c6d6568
solmon <solmon@gmail.com>
2019-10-18 10:12:30 +0530

    Initial Contract Changes

---
+++ contract/index.js
@@ -0,0 +1,10 @@
+/*
+ * Contract Package Export for the user consent
+ */
+
+'use strict';
+
+const SafeConsent = require('./lib/safecon');
+
+module.exports.SafeConsent = SafeConsent;
+module.exports.contracts = [ SafeConsent ];
