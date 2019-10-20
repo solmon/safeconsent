0815b525fa2b3430eaa8c6d7acd9cffefc48c8ef
solmon <solmon@gmail.com>
2019-10-20 17:57:37 +0530

    Removed unused code

--- apps/server/src/app.js
+++ apps/server/src/app.js
@@ -20,45 +20,4 @@ app.post('/createApp', (req, res) => {
     });    
 });
 
-/*
-app.get('/queryAllApps', (req, res) => {
-    network.queryAllCars()
-        .then((response) => {
-            let carsRecord = JSON.parse(response);
-            res.send(carsRecord);
-        });
-});
-
-app.get('/querySingleCar', (req, res) => {
-    console.log(req.query.key);
-    network.querySingleCar(req.query.key)
-        .then((response) => {
-            let carsRecord = JSON.parse(response);
-            res.send(carsRecord);
-        });
-});
-
-app.post('/createCar', (req, res) => {
-    console.log(req.body);
-    network.queryAllCars()
-        .then((response) => {
-            console.log(response);
-            let carsRecord = JSON.parse(response);
-            let numCars = carsRecord.length;
-            let newKey = 'CAR' + numCars;
-            network.createCar(newKey, req.body.make, req.body.model, req.body.color, req.body.owner)
-                .then((response) => {
-                    res.send(response);
-                });
-        });
-});
-
-app.post('/changeCarOwner', (req, res) => {
-    network.changeCarOwner(req.body.key, req.body.newOwner)
-        .then((response) => {
-            res.send(response);
-        });
-});
-*/
-
 app.listen(process.env.PORT || 8081);
